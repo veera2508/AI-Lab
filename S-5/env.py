a63d72a457a105edb226b89ac73a4d8a2e5d2796
Veeraraghavan Narasimhan <veeraraghavan@Veeraraghavans-MacBook-Pro-2.local>
2020-10-05 13:47:29 +0530

    done

--- S-5/env.py
+++ S-5/env.py
@@ -3,17 +3,30 @@ Environment for path finding robot with start location, end location, polygons i
 '''
 import math
 import random
+import itertools
+
+def seq(start, end, step):
+    assert(step != 0)
+    sample_count = int(abs(end - start) / step)
+    return itertools.islice(itertools.count(start, step), sample_count)
 
 
 class point:
     def __init__(self, x: int, y: int):
         self.x = x
         self.y = y
+        self.parent = None
+        self.g = 0
     
     def dist(self, p2) -> int:
         dx = self.x - p2.x
         dy = self.y - p2.y
         return math.sqrt(math.pow(dx,2) + math.pow(dy, 2))
+    
+    def __eq__(self, p2):
+        if (self.x == p2.x and self.y == p2.y):
+            return True
+        return False
     
     @staticmethod
     def genrand(limx: tuple, limy: tuple):
@@ -22,6 +35,13 @@ class point:
         x = random.randint(limx1, limx2)
         y = random.randint(limy1, limy2)
         return point(x, y)
+    
+    def __str__(self):
+        return str(self.x) + str(self.y)
+    
+    def __hash__(self):
+        return hash(str(self))
+
 
 
 class polygon:
@@ -30,7 +50,7 @@ class polygon:
 
     def inside(self, x: int, y: int) -> bool:
         vals = []
-        for i in range(len(points) - 1):
+        for i in range(len(self.points) - 1):
             x1, y1 = self.points[i].x, self.points[i].y
             x2, y2 = self.points[i+1].x, self.points[i+1].y
 
@@ -40,65 +60,74 @@ class polygon:
 
             vals.append(A*x + B*y + C)
         
-        x1, y1 = self.points[len(points) - 1].x, self.points[len(points) - 1].y
+        x1, y1 = self.points[len(self.points) - 1].x, self.points[len(self.points) - 1].y
         x2, y2 = self.points[0].x, self.points[0].y
         A = -(y2-y1)
         B = x2 - x1
         C = -(A*x1 + B*y1)
         vals.append(A*x + B*y + C)
         
-        t1 = all(d>=0 for d in vals)
-        t2 = all(d<=0 for d in vals)
+        t1 = all(d>0 for d in vals)
+        t2 = all(d<0 for d in vals)
         return t1 or t2
 
+class line:
+    def __init__(self, p1, p2):
+        self.x1 = p1.x
+        self.y1 = p1.y
+        self.x2 = p2.x
+        self.y2 = p2.y
+    
+    def gety(self, x):
+        m = (self.y2 - self.y1) / (self.x2 - self.x1)
+        y = m * (x-self.x1) + self.y1
+        return y
+    
+    def isinside(self, polygon):
+        if (self.x1 != self.x2):
+            for x in seq(min(self.x1, self.x2), max(self.x2, self.x1) + 1, 0.1):
+                y = self.gety(x)
+                if (polygon.inside(x, y)):
+                    return True
+            return False
+        else:
+            for y in seq(min(self.y1, self.y2), max(self.y1, self.y2) + 1, 0.1):
+                if (polygon.inside(self.x1, y)):
+                    return True
+            return False
 
 class grid:
     def __init__(self, m: int, n: int, polygons, start: point, goal: point):
-        self.actions = ["u", "d", "l", "r"]
         self.m = m
         self.n = n
         self.polygons = polygons
-        self.current = start
+        self.start = start
         self.goal = goal
-    
-    def isGoal(self) -> bool:
-        return self.start == self.goal
-    
-    def get_actions(self):
-        admissable_actions = []
-        for action in self.actions:
-            x, y = self.current
-            admissable = True
-            if action == "u":
-                y += 1
-            if action == "d":
-                y -= 1
-            if action == "l":
-                x -= 1
-            if action == "r":
-                x += 1
-            if(x < 0 or x >= self.n or y < 0 or y >= self.m):
-                continue
-                
-            for polygon in self.polygons:
-                if polygon.inside(x, y) == True:
-                    admissable = False
-                    break
-            
-            print(x, y, admissable)
-            if admissable:
-                admissable_actions.append(action)
-        return admissable_actions
-    
+        self.points = []
+        for i in polygons:
+            for j in i.points:
+                self.points.append(j)
+        self.points.append(goal)
 
-points = [point(1, 1), point(3, 1), point(3, 3), point(1, 3)]
-p = polygon(points)
-m , n = 5, 5
-start = 0, 0
-end = 4, 5
-g = grid(m, n, [p], start, end)
-print(g.get_actions())
+    def inside(self, l):
+        for polygon in self.polygons:
+            if (l.isinside(polygon)):
+                return True
+        return False
 
+    
+    def getActions(self, state):
+        next_states = []
+
+        for point in self.points:
+            if point != state:
+                if state == point:
+                    continue
+                l = line(state, point)
+                if not self.inside(l):
+                    next_states.append(point)
+        
+        return next_states
 
 
         
